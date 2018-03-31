0c973d46b0c0676d2a22e9a725c581129390757d
millerliang <millerliang@gmail.com>
2018-03-31 12:12:07 +0800

    add timeStamp() for computed

--- js/FroalaEditor.js
+++ js/FroalaEditor.js
@@ -20,19 +20,21 @@ $(function () {
         data: {
             alterClass: '',
             creatNew: {
-                key: '',
+                //key: '',
                 type: '0',
                 title: '',
                 date: '',
                 twYear: '',
-                cont: ''
+                cont: '',
+                editDate: ''
             },
             newItem: {
                 type: '',
                 title: '',
                 date: '',
                 twYear: '',
-                cont: ''
+                cont: '',
+                editDate: ''
             }
         },
         firebase: {
@@ -51,10 +53,24 @@ $(function () {
                     this.newItem.twYear = value.substr(0, 4) - 1911 + '年'
                     // this.twMon = value.replace(/-/g,".").substr(6, 2)
                 }
+            },
+            timeStamp() {
+                const now = new Date() // 建立目前時間容器
+                let date = [now.getFullYear(), now.getMonth() + 1, now.getDate()] // 取得年月日
+                let time = [now.getHours(), now.getMinutes()] // 現在時間
+                let suffix = (time[0] < 12) ? 'AM' : 'PM' // 判斷上下午
+                time[0] = (time[0] < 12) ? time[0] : time[0] - 12
+
+                for (let i = 1; i < 3; i++) {
+                    if (time[i] < 10) {
+                        time[i] = '0' + time[i]
+                    }
+                }
+                return date.join('/') + ' ' + suffix + ' ' + time.join(':')
             }
         },
         methods: {
-            myTitleClass: function () {
+            myTitleClass () {
                 this.alterClass = ""
             },
             newAddItem: function () {
@@ -66,25 +82,26 @@ $(function () {
                 // console.log(this.newItem)
                 // toastr.info('建立新項目')
             },
-            addItem: function () {
+            addItem () {
                 if (this.newItem.title === "") {
                     this.alterClass = "shake is-invalid"
                     toastr.error('標題為必填欄位')
                 } else {
+                    this.newItem.editDate = this.timeStamp
                     this.newItem.cont = $('#froala-editor').froalaEditor('html.get')
                     itemsRef.push(this.newItem)
                     toastr.info('新增成功')
                     $('.editModal').modal('toggle')
                 }
             },
-            editItem: function (item) {
+            editItem (item) {
                 $('#froala-editor').froalaEditor('html.set', item.cont)
                 this.newItem = item
 
                 $('.editModal').modal('show')
                 // toastr.success('編輯這個項目')
             },
-            removeItem: function (item) {
+            removeItem (item) {
                 let checkstr = confirm('確定刪除此項目？')
                 if (checkstr == true) {
                     itemsRef.child(item['.key']).remove()
@@ -95,13 +112,14 @@ $(function () {
                     return false
                 }
             },
-            updateItem: function (item) {
+            updateItem (item) {
                 if (this.newItem.title === "") {
                     this.alterClass = "shake is-invalid"
                     toastr.error('標題為必填欄位')
                 } else {
                     let childKey = item['.key']
                     delete item['.key']
+                    this.newItem.editDate = this.timeStamp
                     this.newItem.cont = $('#froala-editor').froalaEditor('html.get')
                     itemsRef.child(childKey).set(item)
                     // delete childKey
@@ -110,7 +128,7 @@ $(function () {
                     location.reload()
                 }
             },
-            CancelBtn: function () {
+            CancelBtn () {
                 this.alterClass = ''
                 $('.editModal').modal('toggle')
             }
